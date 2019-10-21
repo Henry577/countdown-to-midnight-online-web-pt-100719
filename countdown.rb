d74225f88d9ca24d2262b5b0ea68a2d384382f12
Henry Allan <henryallan50@gmail.com>
2019-10-21 22:10:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,21 @@ def countdown(n)
     
     n -= 1
     
+  end
+  
+  p "HAPPY NEW YEAR!"
+  
+end
+
+
+def countdown_with_sleep(n)
+
+  while n > 0
+  
+    puts "#{n} SECOND(S)!"
+    
+    n -= 1
+    
     sleep(1)
     
   end
